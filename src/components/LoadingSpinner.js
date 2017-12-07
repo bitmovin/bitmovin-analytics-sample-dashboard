b368e6079b2f95bb146db29a8af39c59c8f08b2d
Clemens Helm <clemens.helm@gmail.com>
2017-12-07 20:16:33 +0100

    Resolve CSS naming conflict with LoadingSpinner.

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -5,7 +5,7 @@ class LoadingSpinner extends Component {
 		height: PropTypes.number
 	}
   render () {
-    return <div className="loading" style={{ height: this.props.height }}><div className="inner"><i className="fa fa-spinner fa-pulse fa-fw"></i> <span>Loading...</span></div></div>;
+    return <div className="LoadingSpinner" style={{ height: this.props.height }}><div className="inner"><i className="fa fa-spinner fa-pulse fa-fw"></i> <span>Loading...</span></div></div>;
   }
 }
 export default LoadingSpinner;
