ea527b916ba52a0d9b66498351369e8c38973616
Clemens Helm <clemens.helm@gmail.com>
2017-12-10 21:47:14 +0100

    Add loading indicator to delay page.

--- src/components/TopContentsDelay.js
+++ src/components/TopContentsDelay.js
@@ -3,20 +3,19 @@ import {connect} from 'react-redux';
 import * as startupdelay from '../api/metrics/startupdelay';
 import Card from './Card';
 import VideoLink from './VideoLink';
+import LoadingIndicator from './LoadingIndicator';
 import * as util from '../api/util';
 import ReactPaginate from 'react-paginate';
 
-class TopContents extends PureComponent {
-  constructor (props) {
-    super(props);
-    this.state = {
-      topContents: [],
-      limit: 6,
-      offset: 0,
-      page: 0,
-      orderByOrder: 'DESC'
-    }
-  }
+class TopContentsDelay extends PureComponent {
+  state = {
+    topContents: [],
+    limit: 6,
+    offset: 0,
+    page: 0,
+    orderByOrder: 'DESC',
+    loading: false,
+  };
 
 	componentDidMount () {
 		this.loadData(this.props);
@@ -26,7 +25,9 @@ class TopContents extends PureComponent {
 		this.loadData(nextProps);
 	}
 
-  loadData (props, limit = this.state.limit, offset = this.state.offset, orderByOrder = this.state.orderByOrder) {
+  async loadData (props, limit = this.state.limit, offset = this.state.offset, orderByOrder = this.state.orderByOrder) {
+    this.setState({ loading: true });
+
     const startupDelayQuery = {
       ...props.primaryRange,
       interval: null,
@@ -37,26 +38,24 @@ class TopContents extends PureComponent {
       licenseKey: props.licenseKey
     };
 
-    startupdelay.videoStartupTimeByCountry(props.apiKey, startupDelayQuery).then((data) => {
-      this.setState(prevState => {
-        return {
-          ...prevState,
-          topContents: data,
-          limit,
-          offset,
-          orderByOrder,
-          page: offset / limit
-        };
-      });
+    const data = await startupdelay.videoStartupTimeByCountry(props.apiKey, startupDelayQuery)
+
+    this.setState({
+      topContents: data,
+      limit,
+      offset,
+      orderByOrder,
+      page: offset / limit,
+      loading: false,
     });
   }
 
-  toggleSorting() {
+  toggleSorting = () => {
     const orderByOrder = this.state.orderByOrder === 'DESC' ? 'ASC' : 'DESC';
     this.loadData(this.props, undefined, 0, orderByOrder);
   }
 
-  handlePageClick(pagination) {
+  handlePageClick = (pagination) => {
     const offset = this.state.limit * pagination.selected;
     this.loadData(this.props, this.state.limit, offset);
   }
@@ -70,36 +69,40 @@ class TopContents extends PureComponent {
       tbody = <tbody>{rows}</tbody>;
     }
 
-    return <div>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>Video id</th>
-            <th>Median Startup Delay <i className="fa fa-sort table-metric-sort" aria-hidden="true" onClick={::this.toggleSorting}></i></th>
-          </tr>
-        </thead>
-        {tbody}
-      </table>
-      <ReactPaginate
-        ref="table_pagination"
-        previousLabel={"previous"}
-        nextLabel={"next"}
-        pageCount={300}
-        forcePage={this.state.page}
-        marginPagesDisplayed={0}
-        pageRangeDisplayed={0}
-        onPageChange={::this.handlePageClick}
-        containerClassName={"pagination"}
-        subContainerClassName={"pages pagination"}
-        activeClassName={"active"}/>
-    </div>;
+    return (
+      <LoadingIndicator loading={this.state.loading}>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Video id</th>
+              <th>Median Startup Delay <i className="fa fa-sort table-metric-sort" aria-hidden="true" onClick={this.toggleSorting}></i></th>
+            </tr>
+          </thead>
+          {tbody}
+        </table>
+        <ReactPaginate
+          ref="table_pagination"
+          previousLabel="previous"
+          nextLabel="next"
+          pageCount={300}
+          forcePage={this.state.page}
+          marginPagesDisplayed={0}
+          pageRangeDisplayed={0}
+          onPageChange={this.handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+        />
+      </LoadingIndicator>
+    );
   }
 
   render () {
     return (
       <Card title="Top Contents" width={ this.props.width || {md:4, sm: 4, xs: 12}} cardHeight={"480px"}>
         {this.renderTable()}
-      </Card>);
+      </Card>
+    );
   }
 }
 
@@ -113,4 +116,4 @@ const mapStateToProps = (state) => {
 	}
 };
 
-export default connect(mapStateToProps)(TopContents);
+export default connect(mapStateToProps)(TopContentsDelay);
