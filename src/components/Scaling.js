929d8bd6e4dbf08d209aa0504ee33c47a7c6350c
Clemens Helm <clemens.helm@gmail.com>
2017-12-11 13:34:43 +0100

    Add loading indicator to Quality page.

--- src/components/Scaling.js
+++ src/components/Scaling.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import * as stats from '../api/stats'
-import ReactHighcharts from 'react-highcharts'
-import Card from './Card'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import * as stats from '../api/stats';
+import ReactHighcharts from 'react-highcharts';
+import Card from './Card';
+import LoadingIndicator from './LoadingIndicator';
 
 class Scaling extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      scalingSeries: {
-        data: [],
-        type: 'spline',
-        name: 'Scaling percentage'
-      }
-    }
-  }
+  state = {
+    data: [],
+    loading: true,
+  };
 
   componentDidMount () {
     this.loadData(this.props);
@@ -24,35 +19,26 @@ class Scaling extends Component {
     this.loadData(nextProps);
   }
 
-  loadData(props) {
+  async loadData(props) {
+    this.setState({ loading: true });
+
     const baseQuery = {
       ...props.range,
       interval: props.interval,
       licenseKey: props.licenseKey
     };
-    stats.fetchScalingLastDays(props.apiKey, baseQuery).then((scaling) => {
-      this.setState(prevState => {
-        return {
-          ...prevState,
-          scalingSeries: {
-            ...prevState.scalingSeries,
-            data: scaling
-          }
-        }
-      });
-    });
+    const data = await stats.fetchScalingLastDays(props.apiKey, baseQuery)
 
+    this.setState({ data, loading: false });
   }
   render () {
-    const roundScalingSeries = (series) => {
-      return {
-        ...series,
-        data: series.data.map(row => {
-          row[1] = Math.round(row[1] * 100);
-          return row;
-        })
-      }
-    }
+    const { data, loading } = this.state;
+
+    const roundScalingSeries = (data) => data.map(row => {
+      row[1] = Math.round(row[1] * 100);
+      return row;
+    });
+
     const chartConfig = {
       chart: {
         height: this.props.height
@@ -80,18 +66,25 @@ class Scaling extends Component {
           text: 'Percent'
         }
       },
-      series: [roundScalingSeries(this.state.scalingSeries)],
+      plotOptions: {
+        series: {
+          animation: !loading && { duration: 2000 },
+        },
+      },
+      series: [{ type: 'spline', name: 'Scaling percentage', data: roundScalingSeries(data) }],
       colors: ['#2eabe2', '#35ae73', '#f3922b', '#d2347f', '#ad5536', '#2f66f2', '#bd37d1', '#32e0bf', '#670CE8',
         '#FF0000', '#E8900C', '#9A0DFF', '#100CE8', '#FF0000', '#E8B00C', '#0DFF1A', '#E8440C', '#E80CCE']
     };
     return (
-    <Card title="Scaling Percentage Last 7 Days" width={this.props.width || {md:6, sm: 6, xs: 12}} cardHeight="auto">
-      <div className="x_content">
-        <div className="dashboard-widget-content">
-          <ReactHighcharts config={chartConfig}/>
-        </div>
-      </div>
-    </Card>
+      <Card title="Scaling Percentage Last 7 Days" width={this.props.width || {md:6, sm: 6, xs: 12}} cardHeight="auto">
+        <LoadingIndicator loading={loading}>
+          <div className="x_content">
+            <div className="dashboard-widget-content">
+              <ReactHighcharts config={chartConfig}/>
+            </div>
+          </div>
+        </LoadingIndicator>
+      </Card>
     );
   }
 }
--- src/components/charts/quality/BitrateChart.js
+++ src/components/charts/quality/BitrateChart.js
@@ -1,30 +1,17 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import * as stats from '../../../api/stats'
-import ReactHighcharts from 'react-highcharts'
-import Card from '../../Card'
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as stats from '../../../api/stats';
+import ReactHighcharts from 'react-highcharts';
+import Card from '../../Card';
+import LoadingIndicator from '../../LoadingIndicator';
 
-class QualityChart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      maxBitrateSeries: {
-        data: [],
-        type: 'spline',
-        name: 'Max Bitrate'
-      },
-      minBitrateSeries: {
-        data: [],
-        type: 'spline',
-        name: 'Min Bitrate'
-      },
-      avgBitrateSeries: {
-        data: [],
-        type: 'spline',
-        name: 'Avg Bitrate'
-      }
-    }
-  }
+class BitrateChart extends Component {
+  state = {
+    maxBitRates: [],
+    minBitrates: [],
+    avgBitrates: [],
+    loading: false,
+  };
 
   componentDidMount () {
     this.loadData(this.props);
@@ -34,34 +21,21 @@ class QualityChart extends Component {
     this.loadData(nextProps);
   }
 
-  loadData(props) {
+  async loadData(props) {
+    this.setState({ loading: true });
+
     const baseQuery = {
       ...props.range,
       interval: props.interval,
       licenseKey: props.licenseKey
     };
-    stats.fetchMinMaxAvgBitrate(props.apiKey, baseQuery).then((bitrates) => {
-      this.setState(prevState => {
-        return {
-          ...prevState,
-          maxBitrateSeries: {
-            ...prevState.minBitrateSeries,
-            data: bitrates[0]
-          },
-          minBitrateSeries: {
-            ...prevState.maxBitrateSeries,
-            data: bitrates[1]
-          },
-          avgBitrateSeries: {
-            ...prevState.avgBitrateSeries,
-            data: bitrates[2]
-          }
-        }
-      });
-    });
+    const [maxBitRates, minBitrates, avgBitrates] = await stats.fetchMinMaxAvgBitrate(props.apiKey, baseQuery);
+
+    this.setState({ maxBitRates, minBitrates, avgBitrates, loading: false });
   }
 
   render() {
+    const { maxBitRates, minBitrates, avgBitrates, loading } = this.state;
     const chartConfig = {
       title : {
         text: '',
@@ -84,18 +58,30 @@ class QualityChart extends Component {
           text: 'Bit/sec'
         }
       },
+      plotOptions: {
+        series: {
+          animation: !loading && { duration: 2000 },
+        },
+      },
       tooltip    : {
         shared    : true,
         crosshairs: true
       },
-      series: [this.state.maxBitrateSeries, this.state.minBitrateSeries, this.state.avgBitrateSeries],
+      series: [
+        { type: 'spline', name: 'Max Bitrate', data: maxBitRates },
+        { type: 'spline', name: 'Min Bitrate', data: minBitrates },
+        { type: 'spline', name: 'Avg Bitrate', data: avgBitrates }
+      ],
       colors: ['#2eabe2', '#35ae73', '#f3922b', '#d2347f', '#ad5536', '#2f66f2', '#bd37d1', '#32e0bf', '#670CE8',
         '#FF0000', '#E8900C', '#9A0DFF', '#100CE8', '#FF0000', '#E8B00C', '#0DFF1A', '#E8440C', '#E80CCE']
     };
     return (
       <Card title="Video Bitrate" width={{md:12, sm: 12, xs: 12}} cardHeight={"500px"}>
-        <ReactHighcharts config={chartConfig}/>
-      </Card>);
+        <LoadingIndicator loading={loading}>
+          <ReactHighcharts config={chartConfig}/>
+        </LoadingIndicator>
+      </Card>
+    );
   }
 }
 
@@ -108,4 +94,4 @@ const mapStateToProps = (state) => {
   }
 };
 
-export default connect(mapStateToProps)(QualityChart);
+export default connect(mapStateToProps)(BitrateChart);
