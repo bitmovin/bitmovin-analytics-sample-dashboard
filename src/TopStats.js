fb53976d85335b5e859667788a86439bf98368fc
Clemens Helm <clemens.helm@gmail.com>
2017-12-12 19:04:14 +0100

    Prevent top stats from being reloaded when period popup is opened.

--- src/TopStats.js
+++ src/TopStats.js
@@ -1,79 +1,68 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import * as topstats from './api/topstats'
 import TopStatMetric from './components/TopStatMetric'
 import { push } from 'react-router-redux'
 
-class TopStats extends Component {
-  metricColor(val) {
-    if (val > 0) {
-      return 'green';
-    } else if (val === 0) {
-      return '';
-    }
-    return 'red';
-  }
+function TopStats({ licenseKey, apiKey, primaryRange, secondaryRange, navigateToRebuffer, navigateToPerformance }){
+  const baseQuery = { licenseKey };
 
-  render () {
-    const { licenseKey, apiKey, ranges } = this.props;
-    const baseQuery = { licenseKey };
-
-    return (
-      <div>
-        <div className="row tile_count">
-          <TopStatMetric
-            title="Users"
-            icon="users"
-            fetchData={() => topstats.fetchTotalUsersThisWeek(apiKey, ranges, baseQuery)}
-          />
-          <TopStatMetric
-            title="Impressions"
-            icon="user"
-            fetchData={() => topstats.fetchTotalImpressionsThisWeek(apiKey, ranges, baseQuery)}
-          />
-          <TopStatMetric
-            format="pct"
-            title="Errors"
-            icon="exclamation-triangle"
-            inverse
-            fetchData={() => topstats.fetchErrorPercentageThisWeek(apiKey, ranges, baseQuery)}
-          />
-          <TopStatMetric
-            format="pct"
-            onClick={this.props.navigateToRebuffer}
-            title="Buffering"
-            icon="spinner"
-            compareUserBase
-            inverse
-            fetchData={() => topstats.fetchRebufferPercentageThisWeek(apiKey, ranges, baseQuery)}
-          />
-          <TopStatMetric
-            format="ms"
-            onClick={this.props.navigateToPerformance}
-            title="Delay"
-            icon="clock-o"
-            inverse
-            compareUserBase
-            fetchData={() => topstats.fetchAverageStartupDelayThisWeek(apiKey, ranges, baseQuery)}
-          />
-          <TopStatMetric
-            format="pct"
-            title="Bounce Rate"
-            icon="eject"
-            inverse
-            compareUserBase
-            fetchData={() => topstats.fetchBounceRateThisWeek(apiKey, ranges, baseQuery)}
-          />
-        </div>
+  return (
+    <div>
+      <div className="row tile_count">
+        <TopStatMetric
+          title="Users"
+          icon="users"
+          fetchData={() => topstats.fetchTotalUsersThisWeek(apiKey, primaryRange, secondaryRange, baseQuery)}
+        />
+        <TopStatMetric
+          title="Impressions"
+          icon="user"
+          fetchData={() => topstats.fetchTotalImpressionsThisWeek(apiKey, primaryRange, secondaryRange, baseQuery)}
+        />
+        <TopStatMetric
+          format="pct"
+          title="Errors"
+          icon="exclamation-triangle"
+          inverse
+          fetchData={() => topstats.fetchErrorPercentageThisWeek(apiKey, primaryRange, secondaryRange, baseQuery)}
+        />
+        <TopStatMetric
+          format="pct"
+          onClick={navigateToRebuffer}
+          title="Buffering"
+          icon="spinner"
+          compareUserBase
+          inverse
+          fetchData={() => topstats.fetchRebufferPercentageThisWeek(apiKey, primaryRange, secondaryRange, baseQuery)}
+        />
+        <TopStatMetric
+          format="ms"
+          onClick={navigateToPerformance}
+          title="Delay"
+          icon="clock-o"
+          inverse
+          compareUserBase
+          fetchData={() => topstats.fetchAverageStartupDelayThisWeek(apiKey, primaryRange, secondaryRange, baseQuery)}
+        />
+        <TopStatMetric
+          format="pct"
+          title="Bounce Rate"
+          icon="eject"
+          inverse
+          compareUserBase
+          fetchData={() => topstats.fetchBounceRateThisWeek(apiKey, primaryRange, secondaryRange, baseQuery)}
+        />
       </div>
-    )
-  }
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
 	return {
 		apiKey: state.api.apiKey,
-		ranges: state.ranges,
+		primaryRange: state.ranges.primaryRange,
+		secondaryRange: state.ranges.secondaryRange,
     licenseKey: state.api.analyticsLicenseKey
 	}
 };
