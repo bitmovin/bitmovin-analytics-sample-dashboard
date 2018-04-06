419af7e6536b6663512e4a64c0310f8448155478
Peter Pfaffeneder <peter.pfaffeneder@bitmovin.com>
2018-04-06 15:09:43 +0200

    Applied JavaScript variable naming, fixed minor issue

--- src/services/MetricsCalculation.js
+++ src/services/MetricsCalculation.js
@@ -2,29 +2,26 @@ import * as util from '../api/util';
 
 const COMPLETION_RATE_ACCURACY = 0.1; // percent in decimal format, must not be null.
 
-function isVideoTimeSegmentInChunk(videotime_start, videotime_end, chunktime_start, chunktime_end) {
-  if (videotime_end - videotime_start === 0) { return false; }
-  if (videotime_end < chunktime_start || videotime_start > chunktime_end) {
-    return false;
-  }
-  return true;
+function isVideoTimeSegmentInChunk(videoTimeStart, videoTimeEnd, chunkTimeStart, chunkTimeEnd) {
+  if (videoTimeEnd <= videoTimeStart) { return false; }
+  return !(videoTimeEnd < chunkTimeStart || videoTimeStart > chunkTimeEnd);
 }
 
 export function calculateCompletionRate(impression) {
-  let video_duration = impression[0].video_duration;
+  let videoDuration = impression[0].video_duration;
 
-  if (video_duration === null) { return 'Livestream'; }
-  if(video_duration === 0) { return 'No Data'; }
+  if (videoDuration === null) { return 'Livestream'; }
+  if(videoDuration === 0) { return 'No Data'; }
 
-  let videotime_buckets = [];
-  let video_timesegment = video_duration * COMPLETION_RATE_ACCURACY;
+  let videoTimeBuckets = [];
+  let videoTimeSegment = videoDuration * COMPLETION_RATE_ACCURACY;
   for(var i = 0; i < impression.length; i++) {
     for (var j = 0; j < 1/COMPLETION_RATE_ACCURACY; j++) {
-      let chunktime_start = video_timesegment*j;
-      let chunktime_end = chunktime_start + video_timesegment;
-      videotime_buckets[j] |= isVideoTimeSegmentInChunk(impression[i].videotime_start, impression[i].videotime_end, chunktime_start, chunktime_end);
+      let chunkTimeStart = videoTimeSegment*j;
+      let chunkTimeEnd = chunkTimeStart + videoTimeSegment;
+      videoTimeBuckets[j] |= isVideoTimeSegmentInChunk(impression[i].videotime_start, impression[i].videotime_end, chunkTimeStart, chunkTimeEnd);
     }
   }
-  let completionRate = videotime_buckets.filter(value => value).length * COMPLETION_RATE_ACCURACY;
+  let completionRate = videoTimeBuckets.filter(value => value).length * COMPLETION_RATE_ACCURACY;
   return util.roundTo(completionRate*100, 2) + '%';
 }
