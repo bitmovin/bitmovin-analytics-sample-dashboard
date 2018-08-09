8c2a95f4abefe34610805f15fc283dc67d28d6cc
paulk4633 <pl.kovacic@gmail.com>
2018-08-09 11:18:20 +0200

    Fixed PR Caching for topstats

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,6 @@ import { routerReducer } from 'react-router-redux'
 import api from './api'
 import impressionDetail from './impressionDetail'
 import ranges from './ranges'
-import query from './query'
-import dashboard from './dashboard'
 import topstats from './topstats'
 
 const bitanalytics = combineReducers({
@@ -13,8 +11,6 @@ const bitanalytics = combineReducers({
 	routing: routerReducer,
 	impressionDetail: impressionDetail,
   ranges,
-  query,
-  dashboard,
   topstats
 });
 
